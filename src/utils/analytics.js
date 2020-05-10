7f1eeef5f2f3e3dfab1f25b0def15298e47e4c78
Bennett Lin <bennettslin@gmail.com>
2020-05-10 11:51:18 -0700

    Fix server-side not knowing window.

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,18 +1,21 @@
 /* eslint-disable */
 import { GA_ACCOUNT } from '../constants/analytics'
 
-window.dataLayer = window.dataLayer || []
-function gtag() {
-    dataLayer.push(arguments)
-}
-gtag('js', new Date())
-gtag('config', GA_ACCOUNT)
-/* eslint-enable */
+if (typeof window !== 'undefined') {
 
-if (typeof ga !== 'undefined') {
-    console.log(`GA initialised with property id ${GA_ACCOUNT}.`)
-    ga('create', GA_ACCOUNT, 'auto')
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+        dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
+    gtag('config', GA_ACCOUNT)
+    /* eslint-enable */
 
-} else {
-    console.log('GA did not initialise.')
+    if (typeof ga !== 'undefined') {
+        console.log(`GA initialised with property id ${GA_ACCOUNT}.`)
+        ga('create', GA_ACCOUNT, 'auto')
+
+    } else {
+        console.log('GA did not initialise.')
+    }
 }
