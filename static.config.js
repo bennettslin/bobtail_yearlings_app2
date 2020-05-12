11ab3f7db91c7a5b5e50b242e9fcc2aaeb50c73f
Bennett Lin <bennettslin@gmail.com>
2020-05-11 20:23:33 -0700

    Add favicons.

--- static.config.js
+++ static.config.js
@@ -11,6 +11,30 @@ export default {
                 location: path.resolve('./src/pages')
             }
         ],
+        [
+            /**
+             * This plugin rebuilds the favicon assets every single time, so
+             * it's less than ideal.
+             */
+            'react-static-plugin-favicons',
+            {
+                inputFile: path.resolve('./src/assets/images/bobtailYearlings.png'),
+                outputDir: 'dist/static',
+                configuration: {
+                    icons: {
+                        // Need to list all favicons.
+                        android: false,
+                        appleIcon: false,
+                        appleStartup: false,
+                        coast: false,
+                        favicons: true,
+                        firefox: false,
+                        windows: false,
+                        yandex: false
+                    }
+                }
+            }
+        ],
         require.resolve('react-static-plugin-reach-router'),
         require.resolve('react-static-plugin-sitemap'),
         'react-static-plugin-sass'
