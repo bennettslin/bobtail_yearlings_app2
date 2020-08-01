a6915046e95bb7a96b1f8ded296d1d28bf75ed08
Bennett Lin <bennettslin@gmail.com>
2020-08-01 16:18:01 -0700

    Image always shown upon load.

--- src/components/AlbumLink/index.jsx
+++ src/components/AlbumLink/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import cx from 'classnames'
 
 import Anchor from '../../components/Anchor'
@@ -8,62 +8,52 @@ import albumLink from '../../assets/images/albumLink.png'
 
 import './style.scss'
 
-const Home = () => {
-
-    // Only show the entire component when the image asset is loaded.
-    const [isShown, setIsShown] = useState(false)
-
-    return (
-        <Anchor
+const AlbumLink = () => (
+    <Anchor
+        {...{
+            className: cx(
+                'AlbumLink',
+                'responsive__pageChild'
+            ),
+            href: 'https://www.yearlingsbobtail.com'
+        }}
+    >
+        <Image
             {...{
                 className: cx(
-                    'AlbumLink',
-                    isShown && 'AlbumLink__shown',
-                    'responsive__pageChild'
+                    'AlbumLink__image'
                 ),
-                href: 'https://www.yearlingsbobtail.com'
+                src: albumLink
+            }}
+        />
+        <div
+            {...{
+                className: cx(
+                    'AlbumLink__caption'
+                )
             }}
         >
-            <Image
-                {...{
-                    className: cx(
-                        'AlbumLink__image'
-                    ),
-                    src: albumLink,
-                    onLoad: () => {
-                        setIsShown(true)
-                    }
-                }}
-            />
             <div
                 {...{
                     className: cx(
-                        'AlbumLink__caption'
+                        'AlbumLink__newWebcomic'
                     )
                 }}
             >
-                <div
-                    {...{
-                        className: cx(
-                            'AlbumLink__newWebcomic'
-                        )
-                    }}
-                >
-                    our new audio webcomic
-                </div>
-                <div
-                    {...{
-                        className: cx(
-                            'AlbumLink__yearlingsBobtail',
-                            'Rancho'
-                        )
-                    }}
-                >
-                    <em>{`Yearling's Bobtail`}</em>
-                </div>
+                our new audio webcomic
             </div>
-        </Anchor>
-    )
-}
+            <div
+                {...{
+                    className: cx(
+                        'AlbumLink__yearlingsBobtail',
+                        'Rancho'
+                    )
+                }}
+            >
+                <em>{`Yearling's Bobtail`}</em>
+            </div>
+        </div>
+    </Anchor>
+)
 
-export default Home
+export default AlbumLink
