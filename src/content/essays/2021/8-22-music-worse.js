25fd696b9106e7d56ddd272ef2b662d47df3e567
Bennett Lin <bennettslin@gmail.com>
2021-08-25 01:11:09 -0700

    Update essay.

--- src/content/essays/2021/8-22-music-worse.js
+++ src/content/essays/2021/8-22-music-worse.js
@@ -1,66 +1,69 @@
 export const title = `Music is getting worse`
 export default `
-Is music getting worse? Put me in the camp of those who would say so. To be sure, I only believe this about rock, not hip hop or pop. But few would argue that hip hop and pop are getting better. So if rock is worse, then on average all music is worse.
+Is music getting worse? Put me in the camp of those who would say so. To be sure, I only make this claim about rock, not hip hop or pop. But few would argue that hip hop and pop are getting better. So if rock is worse, then on average all music is worse.
 
 I'm also not claiming that no good rock music is being made today. But if we rank the decades, it can't be a seven-way tie. One of them has to sit at the bottom, and it's hard to imagine it being any decade other than the one we just left.
 
-When people online defend the current state of music, they tend to use the same arguments. So I thought I'd provide a rebuttal to each one of them here.
+When I go online to see what others think about the current state of music, those who defend it tend to use the same set of arguments. So I thought I'd provide a rebuttal to each of their arguments here.
 
 **There are more artists doing more things than ever before.**
 
-Yes, but until there's an apocalypse, every generation can make this claim. For the indefinite future, in every genre and every medium, an ever increasing number of artists will do an ever increasing number of things.
+Yes, but until there's an apocalypse, every generation can make this claim. For the indefinite future, in every genre and every medium, an ever increasing number of artists will be doing an ever increasing number of things.
 
 Yet no one seriously argues that we're living in a golden age of classical or jazz.
 
-**There's amazing music out there if you're willing to find it.**
-
-But this was also true in the past. If you were willing to look, there was a ton of amazing music beyond just the amazing music that everyone today knows about.
-
-Listening to a lesser-known band today, I might think, wow, they're as good as the [Promise Ring](https://en.wikipedia.org/wiki/The_Promise_Ring). Which is good, since the Promise Ring were a good band.
-
-But people today don't talk about the Promise Ring. They talk about Nirvana. Without a Nirvana today, will people tomorrow talk about today's Promise Rings? Or will they just keep talking about Nirvana? I suspect the latter.
-
 **There was bad music in the past as well.**
 
-Of course. But as Lord Acton [once said](https://www.goodreads.com/quotes/617485-judge-talent-at-its-best-but-character-at-its-worst), "Judge talent at its best but character at its worst." In other words, we rate artists by their hits, not their misses. No one knocks Beethoven down a few pegs because he composed [Wellington's Victory](https://en.wikipedia.org/wiki/Wellington%27s_Victory). We're not judging character here.
+Of course. But we rank artists by their hits, not their misses. No one knocks Beethoven down a few pegs because he composed [Wellington's Victory](https://en.wikipedia.org/wiki/Wellington%27s_Victory).
 
-The same rule applies to entire decades as well. Whatever dreck the 60s came up with, they also had the Beatles. Where's today's Beatles?
+The same rule applies to entire decades as well. Whatever dreck the 60s came up with, they also gave us the Beatles. Where's today's Beatles?
+
+**There's so much good music out there if you're willing to find it.**
+
+But this was also true in the past. Beyond just the amazing bands that everyone today knows about, there were tons more if you cared to look. Yo La Tengo, Slint, the Promise Ring… to name a few.
+
+Yet few kids today know about these bands. They talk about today's obscure bands— plus Nirvana. Without a Nirvana today, will kids tomorrow talk about today's obscure bands? Or will they only know about theirs— plus Nirvana?
 
 **Every generation has its naysayers.**
 
 Sure, there will always be grumpy old cranks who think the music of their own time was the best. But when your naysayers are young, you have a problem.
 
-A while back when I taught music theory, I had a casual chat with my students. Some of them seemed to harbor the hope that music only ever seems epic in retrospect. That each generation before had also failed to appreciate what they had until it was gone.
+Back when I taught music theory, I had a casual chat with my students. Some of them seemed to harbor the hope that music only ever seems epic in retrospect. Maybe every generation before had also failed to appreciate what they had until it was gone?
 
-But then I told them how awesome it was to witness the birth of grunge in real time. That when Nirvana exploded onto the scene, it felt like we were watching history in the making. The 60s, 70s, and 80s each had their moment to shine, and now it was our turn.
+But then I told them how awesome it was to witness the birth of grunge as it happened. That when all those Seattle bands exploded onto the scene, it felt like we were watching history in the making. The 60s, 70s, and 80s each had their moment to shine, and now it was our turn.
 
-My students looked crestfallen.
+My students were crestfallen.
 
 **What are the chances that something so reliably good would suddenly become bad in our time?**
 
 What are the chances in *our* time? I don't know. But at *some* point in time? One hundred percent. Because no renaissance ever lasted. Why would rock and roll be any different?
 
-We'd all love to live during a renaissance, yet throughout history, their absence has been the default. So you have to assume they can't be willed into being. They only come about by fortunate circumstances.
+We'd all love to live during a renaissance. Yet throughout history, their absence has been the norm. So you have to assume they can't be willed into being. What's needed is a very fortunate— and very unlikely— set of circumstances.
 
-You also have to assume that they're hard to sustain once the fortune runs out. Because every generation born into a renaissance will take it for granted, and eventually one will let it die. Nothing in their lifetime ever taught them that a renaissance requires active stewardship.
+You also have to assume they're hard to sustain once that fortune runs out. Because every generation born into a renaissance will take it for granted, and eventually one will let it die. Nothing in their lifetime ever taught them that a renaissance requires active stewardship.
 
-But having said that, does anyone today talk about being a responsible steward of the rock and roll renaissance?
+So have you ever heard anyone talk about being a responsible steward of the rock and roll renaissance?
 
 **Judging music by its timbral variety is a bunch of crock.**
 
-Okay, I agree with the defenders on this one. There's all sorts of research these days showing that loudness has gone up over time, while variety in tone and timbre has gone down. None of this proves that music is getting worse.
+Okay, I agree with the defenders on this one. There's all sorts of research these days showing that loudness has gone up over time, while variety in timbre and tone has gone down. This means nothing.
 
-To do that, we don't need data analysis. We need systems thinking. Inefficient systems lead to lower results, and indie rock is an inefficient system by design. Its quantity isn't any lower, so clearly it lowers the quality. And now every band is expected to be indie.
+To prove that music is getting worse, we don't need data analysis. We need systems thinking. Inefficient systems lead to worse results, and the indie system is inefficient by design.
 
-In their heyday, the major labels took big risks to discover bands no one knew about, like Radiohead. By contrast, the indie labels make safe bets on local bands with an established following. They've never promised to do anything else, even after the majors imploded.
+The indie labels require unknown artists to build up a following on their own. This is inefficient, because insiders with greater leverage could advocate for these artists and achieve better results. That's why no other field or genre that cares about quality has this requirement.
 
-By thinking in systems, we can see that the indies won't be discovering the next Radiohead. And they themselves admit they're not trying to.
+In their heyday, the major labels took big risks to discover bands no one knew about, like Radiohead. By contrast, the indies make safe bets on local bands with an established following. They've never promised to do anything else, even after the majors imploded.
+
+By thinking in systems, we can expect indie music to be worse. The indies themselves don't claim to be better. And yet for those who defend today's music, the indie stuff is typically its saving grace.
 
 **What kind of person would rather say that today's music is worse?**
 
-I can't speak for anyone else on my side. But why do I say it? Because I want things to get better. It's like saying the climate is getting worse. You can't fix global warming until you acknowledge that. And when you do, no one reads it as an indictment of today's youth.
+I can't speak for anyone else. But why do I say it? Because I want things to get better. It's like saying the climate has gotten worse. You can't fix global warming until you acknowledge that.
 
-But talk about musical climate change, and suddenly it's the young people who take offense. So let me clear up this matter. Anytime someone says today's music is worse, regardless of their intent, it's always an indictment of previous generations. Not the coming ones.
+And once you do, no one reads it as an indictment of today's youth. But talk about musical climate change, and suddenly it's the youth who take offense. So let me clear this up. When I say music is getting worse, it's an indictment of the previous generations. Not the next ones.
+
+(Expand upon.)
+There are fewer big fish in the ocean.
 
 Now let's fix this thing!
 `
