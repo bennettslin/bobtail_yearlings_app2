e2691343bdc1408960fa33757ac4343034eaca37
Bennett <bennettl@zillowgroup.com>
2021-08-25 20:00:34 -0700

    More essay changes.

--- src/content/essays/2021/8-22-music-worse.js
+++ src/content/essays/2021/8-22-music-worse.js
@@ -1,16 +1,16 @@
-export const title = `Music is getting worse`
+export const title = `Yes, music is getting worse`
 export default `
 Is music getting worse? Put me in the camp of those who would say so. To be sure, I only believe this about rock, not hip hop or pop. But few would argue that hip hop and pop are getting better. So if rock is worse, then on average all music is worse.
 
 I'm also not claiming that no good rock music is being made today. But if we rank the decades, it can't be a seven-way tie. One of them has to sit at the bottom, and it's hard to imagine it being any decade other than the one we just left.
 
-When I go online to see what others think about the current state of music, those who defend it tend to use the same arguments. So I thought I'd provide a rebuttal to each one of them here.
+When I go online to see what others think about the current state of music, those who defend it tend to use the same set of arguments. So I thought I'd provide a rebuttal to each one of them here.
 
 **There are more artists doing more things than ever before.**
 
 Yes, but until there's an apocalypse, every generation can make this claim. For the indefinite future, in every genre and every medium, an ever increasing number of artists will be doing an ever increasing number of things.
 
-Yet no one seriously argues that we're living in a golden age of classical or jazz.
+Yet no one believes that we're still living in the golden age of classical music.
 
 **There was bad music in the past as well.**
 
@@ -20,9 +20,9 @@ The same rule applies to entire decades as well. Whatever dreck the 60s came up 
 
 **There's so much good music out there if you're willing to find it.**
 
-But this was also true in the past. Beyond just the amazing bands that everyone today knows about, there were tons more if you cared to look. Yo La Tengo, Slint, the Promise Ring… to name a few.
+But this was also true in the past. Beyond Nirvana and all the other famous bands that everyone today knows about, there were so many amazing but less popular bands if you cared to look. Yo La Tengo, Slint, the Promise Ring… to name a few.
 
-Yet few kids today know about these bands. They talk about today's obscure bands— plus Nirvana. Without a Nirvana today, will kids tomorrow talk about today's obscure bands? Or will they only know about theirs— plus Nirvana?
+Yet few kids today get into these obscure bands from the past. Why would they? They'd rather listen to their own— and also Nirvana. So why would kids tomorrow bother with today's obscure bands? They'll have plenty of their own as well— plus Nirvana.
 
 **Every generation has its naysayers.**
 
@@ -34,27 +34,33 @@ But then I told them how awesome it was to witness the birth of grunge as it hap
 
 My students were crestfallen.
 
-**What are the chances that something so reliably good would suddenly become bad in our time?**
+**What are the chances that something so good for so long would suddenly turn bad in our time?**
 
 What are the chances in *our* time? I don't know. But at *some* point in time? One hundred percent. Because no renaissance ever lasted. Why would rock and roll be any different?
 
 We'd all love to live during a renaissance. Yet throughout history, their absence has been the norm. So you have to assume they can't be willed into being. What's needed is a very fortunate— and very unlikely— set of circumstances.
 
-You also have to assume they're hard to sustain once that fortune runs out. Because every generation born into a renaissance will take it for granted, and eventually one will let it die. Nothing in their lifetime ever taught them that a renaissance requires active stewardship.
+You also have to assume they're hard to sustain once that fortune runs out. Because every generation born into a renaissance will take it for granted, and eventually one will let it die. Nothing in their lifetime ever taught them that a renaissance requires proper stewardship.
 
-So have you ever heard anyone talk about being a responsible steward of the rock and roll renaissance?
+With that said, have you ever heard anyone talk about being a steward of the rock and roll renaissance?
 
 **Judging music by its timbral variety is a bunch of crock.**
 
-Okay, I agree with the defenders on this one. There's all sorts of research these days showing that loudness has gone up over time, while variety in timbre and tone has gone down. This means nothing.
+Okay, I agree with modern music's defenders on this one. There's some research out there showing that loudness has gone up over time, while variety in timbre and tone has gone down. But to say this proves that music has gotten worse is shallow and weak.
 
-To prove that music is getting worse, we don't need data analysis. We need systems thinking. Inefficient systems lead to worse results, and the indie system is inefficient by design.
+The best analytical arguments don't involve big data. They involve systems thinking. Inefficient systems lead to worse results, and the indie system is inefficient by design.
 
-The indie labels require unknown artists to build up a following on their own. This is inefficient, because insiders with greater leverage could advocate for these artists and achieve better results. That's why no other field or genre that cares about quality has this requirement.
+The indie labels require unknown artists to build up a following on their own. This is inefficient, because insiders with far greater leverage could advocate for these artists and achieve far better results.
+
+That's why no other field or genre that cares about quality has this requirement.
+
+By thinking in systems, we can expect indie music to be worse. And yet for those who defend today's music, the indie stuff is typically its saving grace.
+
+**We would know about today's music.**
+
+The indies themselves don't claim to be better.
 
 In their heyday, the major labels took big risks to discover bands no one knew about, like Radiohead. By contrast, the indies make safe bets on local bands with an established following. They've never promised to do anything else, even after the majors imploded.
-
-By thinking in systems, we can expect indie music to be worse. The indies themselves don't claim to be better. And yet for those who defend today's music, the indie stuff is typically its saving grace.
 
 **What kind of person would rather say that today's music is worse?**
 
