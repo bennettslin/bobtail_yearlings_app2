3a1bdebfa7701483232aba67b6549cc8f6ff4125
Bennett Lin <bennettslin@gmail.com>
2021-08-26 22:54:59 -0700

    More essay notes.

--- src/content/essays/2021/8-22-music-getting-worse.js
+++ src/content/essays/2021/8-22-music-getting-worse.js
@@ -10,7 +10,7 @@ When I go online to see what others think about the current state of music, thos
 
 Yes, but every generation can make this claim. For the indefinite future, in every genre and medium, an ever increasing number of artists will be doing an ever increasing number of things.
 
-And for the past several decades, anyone could have said this about classical or jazz. Yet no one believes these genres are still in the midst of their golden age.
+And for the past several decades, anyone could have said this about classical or jazz. Yet no one believes these genres are still in the midst of a golden age.
 
 **There was bad music in the past as well.**
 
@@ -20,8 +20,9 @@ The same rule applies to entire decades as well. Whatever dreck the 60s came up 
 
 **There's so much good music out there if you're willing to find it.**
 
-(rewrite)
-But this was also true in the past. It wasn't just Nirvana and every other famous band that kids today know about. There were so many amazing but less popular bands if you cared to look. Yo La Tengo, Slint, Cap'n Jazz, the Promise Ring… to name a few.
+(elaborate about famous artists of the past)
+
+But this was also true in the past. Clearly the only choice wasn't just Nirvana and other famous bands that kids know about today. There were so many amazing but less popular bands if you cared to look. Yo La Tengo, Slint, Cap'n Jazz, the Promise Ring… to name a few.
 
 Yet few kids today get into these obscure bands from the past. Why would they? They'd rather listen to their own— along with Nirvana. So why would kids tomorrow bother with today's obscure bands? They can just listen to their own as well— plus Nirvana.
 
@@ -53,11 +54,19 @@ The way to make objective arguments isn't through data analysis. It's with syste
 
 Before signing unknown artists, the indie labels need to see them build a following on their own. This is inefficient, because the labels could stump for these artists and get better results for less effort. Now they're stuck trying to promote artists who've logged more time chasing a following than developing a craft.
 
+(Still don't like this paragraph.)
+
 By thinking in systems, we can expect indie music to be worse. That's why no other genre that cares about quality does things the indie way. Yet for many who defend today's music, the indies are its saving grace!
 
 **Each generation can only be judged by what was possible in its own time.**
 
+(Explain rationale of argument, punk example. Achille's heel if this argument is that it assumes that if something revolutionary came on, we would know about it.)
 
+(No one's trying to discover next Radiohead. Not the majors, not the indies.)
+
+(Yearling's Bobtail. Same time, energy, and skill level in graphic novel, some publisher would have picked it up.)
+
+(I'm not saying. YB is the canary in the coalmine.)
 
 The indies themselves don't claim to be better.
 
@@ -70,6 +79,7 @@ I can't speak for everyone. But why do I say it? Because I want things to get be
 And once you do, no one reads it as an indictment of today's youth. But talk about musical climate change, and suddenly it's the youth who take offense. So let me clear this up. When I say music is getting worse, it's an indictment of previous generations. Not the next ones.
 
 (Expand upon.)
+
 There are fewer big fish in the ocean.
 
 Now let's fix this thing!
