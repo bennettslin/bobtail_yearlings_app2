5f8e5d5361645e0d8c27a54e12976564faee3401
Bennett Lin <bennettslin@gmail.com>
2021-09-05 18:43:56 -0700

    Text follow buttons.

--- src/components/FollowButtons/index.jsx
+++ src/components/FollowButtons/index.jsx
@@ -12,9 +12,7 @@ const FollowButtons = () => (
             ),
         }}
     >
-        <label {...{ className: 'FollowButtons__label' }}>
-            Follow us:
-        </label>
+        <label>Follow us:</label>
         <FacebookButton />
         <TwitterButton />
     </div>
