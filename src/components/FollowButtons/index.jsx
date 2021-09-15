cba684a4edcee49e57f38672c9d0b9cb78609c21
Bennett Lin <bennettslin@gmail.com>
2021-09-14 23:11:33 -0700

    Add flex component.

--- src/components/FollowButtons/index.jsx
+++ src/components/FollowButtons/index.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import cx from 'classnames'
+import Flex from '../Flex'
 import FacebookButton from './FollowButton/FacebookButton'
 import TwitterButton from './FollowButton/TwitterButton'
 import './style'
 
 const FollowButtons = () => (
-    <div
+    <Flex
         {...{
             className: cx(
                 'FollowButtons',
             ),
         }}
     >
-        <label>Follow us:</label>
+        <label>Follow the Bobtail Yearlings:</label>
         <FacebookButton />
         <TwitterButton />
-    </div>
+    </Flex>
 )
 
 export default FollowButtons
