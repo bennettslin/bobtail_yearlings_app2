f0c4fc10261eaca9453b7fba6a2305e78ca47b17
Bennett Lin <bennettslin@gmail.com>
2021-10-09 05:45:26 -0700

    More essay work.

--- src/pages/essays/2021/8-22-music-getting-worse.jsx
+++ src/pages/essays/2021/8-22-music-getting-worse.jsx
@@ -11,25 +11,25 @@ const Component = () => (
             body: `
 Is music getting worse? Put me in the camp of those who say yes. To be sure, I only say this about rock, not hip hop or pop. But few would argue that hip hop and pop are getting better. So if rock is worse, then on average all music is worse.
 
-I'm also not claiming that no good rock music is being made today. But if we rank the decades, it can't be a seven-way tie. One of them has to sit at the bottom, and it's hard to imagine it being any decade other than the one we just left.
+I'm also not claiming that no good rock music is being made today. But if we rank the decades, it can't be a seven-way tie. One of them has to sit at the bottom, and it's hard to imagine it being any decade other than the one that just passed.
 
 When I go online to see what others think about the current state of music, those who defend it tend to use the same set of arguments. So I thought I'd provide a rebuttal to each one of them here.
 
-**What kind of sad person would rather think that music is getting worse?**
+**What kind of sad person would rather say that music is getting worse?**
 
-This one is implied rather than stated. Music is subjective, the thinking goes, and so whether it's getting worse just depends on your state of mind. If you're a cynic, it's because you choose to be.
+This one is implied rather than stated. Music is hopelessly subjective, the thinking goes, and so whether it's getting worse just depends on your state of mind. If you're a cynic, it's because you choose to be.
 
-But this misunderstands my motive. Why do *I* say that today's music has gotten worse? Because I want us all to fix it. It's like global warming. You can't fix it until you acknowledge that it's happening.
+But this misunderstands my motive. Why do *I* say that today's music has gotten worse? Because I want us all to fix it. It's like global warming. You can't fix global warming until you acknowledge it's happening.
 
-And once you do, no one assumes you're blaming today's youth for all the floods and wildfires. But talk about musical climate change, and it's the youth who take offense. So let me clear this up. When I say that music is getting worse, it's an indictment of my generation. Not theirs.
+And then once you do, no one assumes you're blaming today's youth for all the floods and wildfires. But talk about musical climate change, and it's the youth who take offense. So let me clear this up. When I say music is getting worse, it's an indictment of my generation. Not theirs.
 
-Previous generations depleted the oceans of all the big fish. And my generation exhausted rock music of any more big names. We broke it, so it's on us to fix it.
+Previous generations depleted the oceans of all the big fish. And my generation exhausted rock music of any more big names. It's our mess, so it's on us to clean it up.
 
 **There are more artists doing more things than ever before.**
 
-Yes, but every generation can make this claim. For the indefinite future, in every genre and medium, an ever increasing number of artists will be doing an ever increasing number of things.
+Yes, but every generation can make this claim. For the indefinite future, in every genre and every medium, an ever increasing number of artists will be doing an ever increasing number of things.
 
-And for the past several decades, anyone could have said this about classical or jazz. Yet no one believes these genres are still in the midst of their golden age.
+Anyone today can say this about classical or jazz, for example. Yet no one believes these genres are still in the midst of their golden age.
 
 **There was bad music in the past as well.**
 
@@ -39,15 +39,13 @@ The same rule applies to entire decades as well. Whatever dreck the 60s came up 
 
 **There's so much good music out there if you're willing to find it.**
 
-(TODO: 1. elaborate about famous artists of the past)
+(TODO: Tie this up nicely.)
 
 But this was just as true in the past. It's not like my generation only had Nirvana and Pearl Jam and no one else. There were plenty of amazing but less popular bands if you cared to look. Yo La Tengo, Slint, Cap'n Jazz, the Promise Ring… to name a few.
 
 Yet few young people today get into these obscure bands from the past. Why would they? They'd rather listen to their own— along with Nirvana. So why would young people tomorrow bother with today's obscure bands? They can just listen to their own as well— along with Nirvana.
 
-(TODO: Present day bands are not good to future fans.)
-
-As someone who grew up listening to music made by people with different values who'd been dead for centuries, I've never understood.
+It's not superficial to weight the big names. History doesn't care about giving each generation fair representation. History skips over some entirely.
 
 **Every generation has its naysayers.**
 
@@ -65,49 +63,33 @@ What are the chances in *our* time? I don't know. But at *some* point in time? O
 
 We'd all love to live during a renaissance. Yet throughout history, their absence has been the norm. So you have to assume they can't be willed into being. What's needed is a very fortunate— and very unlikely— set of circumstances.
 
-You also have to assume they're hard to sustain once that fortune runs out. Because every generation born into a renaissance will take it for granted, and eventually one will let it die. Nothing in their lifetime ever taught them that a renaissance requires proper stewardship.
+You also have to assume they're hard to sustain once that fortune runs out. Because every generation born into a renaissance ends up taking it for granted, and eventually one will let it die. Nothing in their lifetime ever taught them that a renaissance requires actual stewardship.
 
 With that said, have you ever heard anyone talk about being a steward of the rock and roll renaissance?
 
 **Measuring music by its timbral variety is a bunch of crock.**
 
-Okay, I agree with the defenders on this one. Yes, research based on analyzing thousands of popular songs over the years has shown that tonal and timbral variety has gone down over time. But by what supreme authority does this actually mean today's music is worse?
+Okay, I agree with the defenders on this one. Yes, research based on analyzing thousands of popular songs over the years has shown that tonal and timbral variety has gone down over time. But by what supreme authority does this mean today's music is worse?
 
-Music is too subjective, and so you can't make objective arguments with data analysis. You need systems thinking instead. For example, few would dispute that inefficient systems lead to worse results. Well, the indie system is inefficient by design.
+Music is hopelessly subjective, so you can't make objective arguments with data analysis. You need systems thinking instead. For example, few would dispute that inefficient systems lead to worse results. Well, the indie system is inefficient by design.
 
 The indie labels only sign artists who've built a fanbase on their own. But a label has leverage while an unknown artist has none. And more leverage gets you better results for the same effort. So in an efficient system, building the fanbase would be a label's job.
 
-Instead, the labels are stuck trying to promote artists who've logged more time chasing a following than developing a craft. This gradually loses them prestige. Which then compels them to find artists who can do even more of their work for them, and the vicious circle continues.
+Instead, the indie labels are stuck trying to promote artists who've logged more time chasing a following than developing a craft. They've lost some prestige as a result. Which then compels these labels to find artists who can do even more of their work for them, and the vicious circle continues.
 
 By thinking in systems, we can expect indie music to be worse. Yet for many who defend today's music, the indies are its saving grace!
 
 **Each generation can only be judged by what's possible in its own time.**
 
-Previous generations plucked all the low-hanging fruit. No band as raw as the Sex Pistols can ever again change the world. So the argument here is that today's artists haven't fumbled the ball. The means to start the next revolution are now too high to reach.
+(TODO: Wrap this up.)
 
-But this assumes that we're doing our best to find the revolutionary artists out there. That once they arrive, you'll definitely hear about them. Prove this assumption wrong, and the argument falls apart. So let's do that.
+Previous generations plucked all the low-hanging fruit. No band as raw as the Sex Pistols will ever again change the world. So the argument here is that today's artists haven't fumbled the ball. The means to start the next revolution are now too high to reach.
 
-Here's the first part of my rebuttal. In their heyday, the major labels took big risks to discover bands no one knew about, like Radiohead.
+But this assumes that we're doing our best to find the revolutionary artists out there. That once they arrive, you'll definitely hear about them. Why would we just assume that?
 
-By contrast, the indie labels make safe bets on local bands with an established following. And they've never promised to do anything else.
+In their heyday, the major labels took big risks to discover bands no one knew about, like Radiohead. By contrast, the indie labels make safe bets on local bands with an established following. They've never promised to do anything else, even after the majors imploded.
 
-(TODO: Risk averse.)
-
-The majors have become risk-averse.
-But you have to assume that discovering a great band isn't possible without taking big risks.
-
-(TODO: No one's trying to discover next Radiohead. Not the majors, not the indies.)
-And so the next Radiohead can never be discovered. Or, more likely, the band that could have been the next Radiohead in some parallel universe, instead does things the indie way. And that's what they pride themselves on, never knowing how much their fate was decided by the system.
-
-(TODO: Yearling's Bobtail. Same time, energy, and skill level in graphic novel, some publisher would have picked it up.)
-
-(TODO: I'm not saying. YB is the canary in the coalmine.)
-
-You may not have any interest in the Ulysses of rock albums. But it's the canary in the coalmine.
-
-Demo that can't be refused.
-
-The indies themselves don't claim to be better.
+Revolutions are hard. They involve taking risk. They disrupt livelihoods, people in power. Hair metal and grunge.
             `,
         }}
     />
