6664cb63ce336a43421f3eb4ace4ce4c833f9d22
Bennett Lin <bennettslin@gmail.com>
2021-10-30 21:44:24 -0700

    Link replaces location.

--- src/content/essays/2021/index.js
+++ src/content/essays/2021/index.js
@@ -2,11 +2,11 @@ import { getMapFromPageConfigs } from '../../../utils/pages/config'
 import { ESSAYS_PAGE } from '../../../constants/pages'
 
 export const pageConfigs = [
-    {
-        id: `music-getting-worse`,
-        title: `Music is getting worse`,
-        date: { year: 2021, month: 8, day: 22 },
-    },
+    // {
+    //     id: `music-getting-worse`,
+    //     title: `Music is getting worse`,
+    //     date: { year: 2021, month: 8, day: 22 },
+    // },
     {
         id: `old-links`,
         title: `Old links`,
