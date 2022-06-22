21d4561140a1c576d147f682bbd09e098753049c
Bennett Lin <bennettslin@gmail.com>
2022-06-22 13:13:42 -0700

    Minor change.

--- src/content/essays/2022/index.js
+++ src/content/essays/2022/index.js
@@ -3,14 +3,14 @@ import { ESSAYS_PAGE } from '../../../constants/pages'
 
 export const pageConfigs = [
     {
-        id: `original-bycombo-story`,
-        title: `Original BYCombo story`,
-        date: { year: 2022, month: 5, day: 6 },
-    },
-    {
         id: `farewell-to-zillow`,
         title: `Farewell to Zillow`,
         date: { year: 2022, month: 6, day: 20 },
+    },
+    {
+        id: `original-bycombo-story`,
+        title: `Original BYCombo story`,
+        date: { year: 2022, month: 5, day: 6 },
     },
 ]
 
