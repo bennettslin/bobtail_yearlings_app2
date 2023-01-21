f5d896738e7446e169522d008de260f7dfbdf4d7
Bennett Lin <bennettslin@gmail.com>
2023-01-20 21:41:49 -0800

    Ongoing work.

--- src/pages/essays/ongoing/band-mission-and-principles.jsx
+++ src/pages/essays/ongoing/band-mission-and-principles.jsx
@@ -9,35 +9,69 @@ const Component = () => (
         {...{
             ...pagesMap[id],
             body: `
-### Our mission
+**The Bobtail Yearlings promise to become the most historically consequential rock band of the 21st century. To do so, we'll lead the interdependence revolution while amassing a solid body of creative works.**
 
-**The Bobtail Yearlings are committed to being the most historically consequential rock band of the 21st century! We'll do so by embodying the core values of interdependence and self-sacrifice while amassing a solid body of creative works.**
+The following guiding principles will help us fulfill this mission.
 
-### Our guiding principles
+### Historical consequence
 
-##### Historical consequence
+${'  ' || `
+// TODO: Make link a target blank.
+`}
 
-##### Interdependence
+From here on out, making rock history will require deliberate effort. And so every decision we make will be informed by our [five vowels of historical consequence](/essays/ongoing/five-vowels-of-historical-consequence).
 
-Blah
+### Interdependence
 
-##### Emotional intelligence
+The best art is made by interdependent artists— and this has always been the case. We'll lead the 
 
-Blah
 
-##### Member compensation
+For this reason, we'll work closely with BYCombo,  help create interdependent systems to benefit both ourselves and other bands.
 
-Blah
+### Emotional intelligence
 
-##### Member responsibilities
+Being in the Bobtail Yearlings should be a meaningful and rewarding experience. As such, each member will vow to be a paragon of emotional intelligence. Naturally, this isn't binding but simply an act of good faith, which will serve as a permanent reminder to treat others with empathy and respect.
 
-Blah
+### Member recruitment
 
-##### Admin
+While the Bobtail Yearlings won't choose new members on the basis of race or gender, the benefits of diversity, as well as the dangers of unconscious bias, will be openly discussed for each prospect.
 
-##### Future growth
 
-##### Self-sacrifice
+
+More isn't always better… but it's always *more*.
+Our founding member 
+
+### Member roles
+
+There will be no permanent leader, only temporary project managers. For each project, one or a few members will be in charge of artistic development.
+
+One additional member will be a full-time admin. The admin will be in charge of all administrative work, such as social media engagement.
+
+
+
+### Member duties
+
+Each member will work at least forty hours a week to benefit the band, for the duration of their time as a member. This work can largely be self-directed. Examples include practicing one's instrument, writing song parts, and so forth.
+
+Naturally, touring and performing count as band work. Some projects will also require members to undertake specific tasks. In such cases, the project manager will work with these members to ensure that expectations are reasonable, and that needed resources are provided.
+
+Members are exempt from working on holidays, vacations, and sick days.
+
+### Compensation
+
+Every member who participates in the creation of a project will get equal credit and royalties. This includes the admin.
+
+${'  ' || `
+// TODO: Italicize board games?
+`}
+
+Ongoing revenue from the band's extramusical projects, such as [Bobtail Dominoes](https://bobtaildominoes.com) and [*Bobtail Method*](https://bobtailmethod.com), will first go to new members to grow their savings, up to a fixed limit. Any leftover amount will then go into a general band fund.
+
+### Future growth
+
+
+
+### Self-sacrifice
 
 Blah
     `,
--- src/pages/essays/ongoing/five-vowels-of-historical-consequence.jsx
+++ src/pages/essays/ongoing/five-vowels-of-historical-consequence.jsx
@@ -9,9 +9,11 @@ const Component = () => (
         {...{
             ...pagesMap[id],
             body: `
-*Updated November 2022*
 
-A long time ago, a friend showed me the album he'd made with his band while in high school. When I asked where he expected it to go in fifty years, he replied, "Why would I care?" This floored me. As someone who grew up on Beethoven and the Beatles, I just assumed that we all make music for posterity— some, like myself, are just more committed to succeeding than others.
+*This is a work in progress.*
+
+${'  ' || `
+// TODO: A long time ago, a friend showed me the album he'd made with his band while in high school. When I asked where he expected it to go in fifty years, he replied, "Why would I care?" This floored me. As someone who grew up on Beethoven and the Beatles, I just assumed that we all make music for posterity— some, like myself, are just more committed to succeeding than others.
 
 But my friend clued me in on another attitude. Others saw making music as just a way to express themselves, to hang out with friends and feel alive. The professional look of the CD just added to the fun, not a signal of serious intent. And, of course, the joke was on me. Indie culture is all about celebrating and elevating artists like my friend, while ignoring artists like me.
 
@@ -22,37 +24,48 @@ Yet after twenty years of indie culture reigning supreme, yet mattering very lit
 But what does it mean to make history? As someone who's been thinking about it all my life, here are my guesses.
 
 Using as an example Yearling's Bobtail, my audio webcomic for the Ulysses of rock albums. Obviously, no one today even knows about YB, so I'm not using it as an example that has succeeded. I'm just using it as an example of what someone who thinks the way I do would create, based on this very thinking.
+`}
 
-### A is for academic
+### A for academic
 
-It can be taught by the academy.
+${'  ' || `
+// TODO: It can be taught by the academy.
 Yearling's Bobtail.
 
 But anything that can be taught will eventually be institutionalized by the academy, even if it's considered too low-brow by today's standards. Jazz is the obvious example.
 So if as an artist, you want to study it for the benefit of your own self-education, it also counts.
 In other words, when I would rewind a Def Leppard tape or a Nirvana CD to figure out what they were doing.
+`}
 
-### E is for enjoyable
+### E for enjoyable
 
-Enjoyable for others.
+${'  ' || `
+// TODO: Enjoyable for others.
 
 Enjoyable for self.
 I may ruin a song with obtuse lyrics. But at its core, every song I've ever written, at its core, is a melody and chord progression that I enjoy just humming and strumming, and just working on as it stays in this stage for months, and even years.
 Just paying attention to the interplay, as the melody and chords push and pull away from each other.
+`}
 
-### I is for inspiring
+### I for inspiring
 
-I've never inspired anyone in my life.
+${'  ' || `
+// TODO: I've never inspired anyone in my life.
+`}
 
-### O is for original
+### O for original
 
-Nothing that has to do with how music is marketed or distributed will matter to history.
+${'  ' || `
+// TODO: Nothing that has to do with how music is marketed or distributed will matter to history.
 
 Can't be commissioned.
+`}
 
-### U is for universal
+### U for universal
 
-Speaks to the universal human experience.
+${'  ' || `
+// TODO: Speaks to the universal human experience.
+`}
     `,
         }}
     />
