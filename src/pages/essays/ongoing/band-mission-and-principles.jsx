ba784c08ad1c34cf4b303db34a01a361f85b0b0a
Bennett Lin <bennettslin@gmail.com>
2023-01-23 04:58:10 -0800

    More work.

--- src/pages/essays/ongoing/band-mission-and-principles.jsx
+++ src/pages/essays/ongoing/band-mission-and-principles.jsx
@@ -17,27 +17,25 @@ const Component = () => (
 
 ##### Historical consequence
 
-In the early days of rock, an artist could accidentally stumble into the history books. But from here on out, making history will require deliberate effort. And so every decision the Bobtail Yearlings make will be informed by the [five vowels of historical consequence](https://bobtailyearlings.com/essays/ongoing/five-vowels-of-historical-consequence).
+In the early days of rock, an artist could accidentally stumble into the history books. But from here on out, making history will require deliberate effort. And so every decision the Bobtail Yearlings make will be informed by our [five vowels of historical consequence](https://bobtailyearlings.com/essays/ongoing/five-vowels-of-historical-consequence).
 
 ##### Longevity
 
-In classical music, the best composers got *better* with age. For rock to remain relevant as a musical genre, it must reach this next level of maturity. As such, the Bobtail Yearlings expect to be a working band for at least as long as Bennett is alive, with our best works always ahead of us.
+In classical music, the best composers got *better* with age. For rock to remain relevant as a musical genre, it must reach this next level of maturity. As such, the Bobtail Yearlings plan to be a working band for at least as long as Bennett is alive, with our best works always ahead of us.
 
 ##### Interdependence
 
-The best artists of the past were interdependent. That is to say, they were allowed to focus solely on their craft, while a partner handled their business affairs. And so the Bobtail Yearlings will work closely with [BYCombo](https://bycombo.com), our band incubator, to make the system work again for ourselves and our interdependent allies.
+The best artists of the past were interdependent. That is to say, they were allowed to focus solely on their craft, while a partner handled their business affairs. And so the Bobtail Yearlings will work closely with [BYCombo](https://bycombo.com), our band incubator, to make the system work again for interdependent bands like ourselves.
 
 ##### Business partners
 
-Studies show that even though our culture teaches us to admire creativity, it's actually human nature to dislike it— and [experts reject creativity](https://www.theatlantic.com/business/archive/2014/10/why-new-ideas-fail/381275) more than anyone else.
+Studies show that humans actually dislike creativity— and [experts more so](https://www.theatlantic.com/business/archive/2014/10/why-new-ideas-fail/381275) than anyone else. This is why, time and time again, the business partners who purport to care most about putting out great art— who consider themselves experts in knowing what great art looks like— end up putting out the least creative works.
 
-This is why, again and again, the most creative works are put out by business partners who are clueless about art, who just want to make money. Meanwhile, the ones who care most about putting out great art— who pride themselves on knowing what great art looks like— tend to put out works of little consequence.
-
-For this reason, the Bobtail Yearlings prefer to work with business partners who are experts in making money, not in recognizing great art.
+Meanwhile, some of the most creative works are put out by business partners who are completely naive about art. For this reason, the Bobtail Yearlings prefer to work with business partners who take pride in knowing how to do business, not in recognizing great art.
 
 ##### Emotional intelligence
 
-Everyone should find it enjoyable and rewarding to interact with the Bobtail Yearlings. And it should feel just as meaningful for us to work with others, as well as with each other. So each Yearling will vow to be a model of emotional intelligence, who'll always treat others with empathy and respect.
+If the Bobtail Yearlings are to fulfill our mission, then everyone should find it enjoyable and rewarding to interact with us. And it should feel just as meaningful for us to work with others, as well as with each other. So each Yearling will vow to be a model of emotional intelligence, who'll always treat others with empathy and respect.
 
 ##### Recruitment
 
