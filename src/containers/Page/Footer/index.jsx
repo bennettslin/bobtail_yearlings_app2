84412104089fbc36e3c090fe1925f5b1efa1aa80
Bennett Lin <bennettslin@gmail.com>
2023-01-31 16:00:24 -0800

    Remove direction links.

--- src/containers/Page/Footer/index.jsx
+++ src/containers/Page/Footer/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import cx from 'classnames'
-import DirectionRow from './DirectionRow'
-import './style'
 import TopLevelShareRow from './TopLevelShareRow'
+import './style'
 
 const PageFooter = () => (
     <div
@@ -12,7 +11,6 @@ const PageFooter = () => (
             ),
         }}
     >
-        <DirectionRow />
         <TopLevelShareRow />
     </div>
 )
