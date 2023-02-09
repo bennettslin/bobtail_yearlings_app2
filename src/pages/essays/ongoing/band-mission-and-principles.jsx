74745c54d1ba7193d6e2540372b6cd1374385e23
Bennett Lin <bennettslin@gmail.com>
2023-02-08 21:54:19 -0800

    Work on guiding principles.

--- src/pages/essays/ongoing/band-mission-and-principles.jsx
+++ src/pages/essays/ongoing/band-mission-and-principles.jsx
@@ -29,13 +29,19 @@ To set an example for future generations, the Bobtail Yearlings promise to be a 
 
 ##### Interdependence
 
-To keep more of their earnings for themselves, indie artists divert time and energy toward administrative work that would otherwise be handled by business partners. Some of them also forgo having bandmates, allowing them not to split their earnings further.
+To avoid sharing their earnings, indie artists spend time and energy on administrative work that would otherwise be handled by business partners. And many take this a step further, by choosing to create without collaboration. But what competitive edge could a solo artist working part time possibly have, to make them the best in the *world*?
 
-But what competitive edge could a part-time, solo artist possibly have that would make them the best in the *world*?
+As such, the Bobtail Yearlings will readily work with business partners, allowing us to focus solely on our craft. And we'll periodically bring in new bandmates to be equal collaborators, allowing us to create works greater than the sum of their parts.
 
-As such, the Bobtail Yearlings will readily work with business partners, allowing us to focus solely on our craft. We'll also periodically bring in new Yearlings, including artists in other mediums, allowing us to create works greater than the sum of their parts.
+${'  ' || `
+// TODO: Make this more clear. Talk about Give and Take.
+
+##### Self-sacrifice
+
+While the five vowels of historical consequence are timeless, expectations differ for each generation. This is why some will start a renaissance, while others will let one end.
 
 And we'll work closely with [BYCombo](https://bycombo.com), our band incubator, to make the system work again for other bands who desire interdependence like we do.
+`}
 
 ##### Business partners
 
@@ -45,7 +51,7 @@ Meanwhile, the *most* creative works are often backed by those who are completel
 
 ##### Emotional intelligence
 
-If the Bobtail Yearlings are to achieve our mission, then others should find it pleasant and rewarding to interact with us. And it should feel just as meaningful for us to work with each other. So each Yearling will vow to be a model of emotional intelligence, who'll treat others with empathy, kindness, and respect.
+If the Bobtail Yearlings are to achieve our mission, then others should find it pleasant and rewarding to interact with us. And it should feel just as meaningful for us to work with each other. So each of us will vow to be a model of emotional intelligence, who'll treat others with empathy, kindness, and respect.
 
 Of course, we won't hesitate to criticize indie rock as an ideology. But we'll always remind ourselves that those who've devoted their lives to it are people like us, and they're the heroes of their own stories.
 
@@ -53,29 +59,33 @@ Of course, we won't hesitate to criticize indie rock as an ideology. But we'll a
 
 The band leader decides on the overall vision for the Bobtail Yearlings. For now, Bennett is our band leader, since this is the de facto reality. However, in the long term we should be open to having multiple band leaders, or even none at all.
 
-For each project, one Yearling will be assigned the role of project manager. The project manager will oversee artistic development of that project.
+${'  ' || `
+// TODO: Mention artists who work in other mediums.
+`}
 
-One Yearling will be a full-time admin, in charge of administrative work such as social media engagement. This will shield every other Yearling from such responsibilities. The admin will also oversee development of our extracurricular projects, such as [*Bobtail Dominoes*](https://bobtaildominoes.com), [*Bobtail Method*](https://bobtailmethod.com), and of course, BYCombo.
+For each project, one bandmate will be assigned the role of project manager. The project manager will oversee artistic development of that project.
+
+One bandmate will be a full-time admin, in charge of administrative work such as social media engagement. This will shield every other bandmate from such responsibilities. The admin will also oversee development of our extracurricular projects, such as [*Bobtail Dominoes*](https://bobtaildominoes.com), [*Bobtail Method*](https://bobtailmethod.com), and of course, BYCombo.
 
 ##### Duties
 
-Every Yearling will work forty hours a week to benefit the band, for the duration of their time in the band. This work will largely be self-directed. Examples include practicing one's instrument, writing song parts, and so forth. Naturally, touring and performing count as band work.
+Each Yearling will work forty hours a week to benefit the band, for the duration of their time in the band. This work will largely be self-directed. Examples include practicing one's instrument, writing song parts, and so forth. Naturally, touring and performing count as band work.
 
-Some projects will require certain Yearlings to undergo extensive artistic development, such as learning a new skill set. In such cases, the project manager will work with them to ensure that expectations are reasonable, and needed resources are provided.
+Some projects will require certain bandmates to undergo extensive artistic development, such as learning a new skill set. In such cases, the project manager will work with them to ensure that expectations are reasonable, and needed resources are provided.
 
 Of course, we'll take holidays, vacations, and sick days off.
 
 ##### Compensation
 
-Everyone who's a Yearling during the creation of a project will be given equal credit and royalties. This includes the admin, as well as artists in other mediums. Of course, we're free to mention who contributed which parts, as Lennon and McCartney often did.
+Everyone who's a Yearling during the creation of a project will be given equal credit and royalties. This includes the admin, as well as artists of other mediums. Of course, we're free to mention who contributed which parts, as Lennon and McCartney often did.
 
-Ongoing earnings from our extracurricular projects will first go to new Yearlings to grow their savings, if needed, up to a fixed limit. Any leftover amount will then go into our general band fund.
+Ongoing earnings from our extracurricular projects will first go to new bandmates to grow their savings, if needed, up to a fixed limit. Any leftover amount will then go into our general band fund.
 
 ##### Recruitment
 
-While we won't select new Yearlings on the basis of race or gender, we'll openly address the benefits of diversity and the hazards of unconscious bias.
+While we won't select new bandmates on the basis of race or gender, we'll openly address the benefits of diversity and the hazards of unconscious bias.
 
-We also understand the need to juggle band life with adult responsibilities. So if bringing a life partner into the band will help a Yearling maintain their commitment, then we'll do our best to make this work.
+We also understand the need to juggle band life with adult responsibilities. So if bringing a life partner into the band will help a bandmate maintain their commitment, then we'll do our best to make this work.
     `,
         }}
     />
