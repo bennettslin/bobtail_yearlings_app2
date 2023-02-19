2063b06511d657354948acceaa9089c769fe51af
Bennett Lin <bennettslin@gmail.com>
2023-02-18 16:26:49 -0800

    Add section for helping others to mission.

--- src/pages/essays/ongoing/band-mission-and-principles.jsx
+++ src/pages/essays/ongoing/band-mission-and-principles.jsx
@@ -19,7 +19,7 @@ const Component = () => (
 
 In the past, the rock music of the English-speaking world was [*the* canon](https://www.rollingstone.com/music/music-lists/100-greatest-artists-147446). It was always the most original, and often the most accomplished. But today's indie rock isn't better than any foreign canon out there, which is why it seems so inconsequential.
 
-To make history once again, bands need to be the best in the *world*— and this requires deliberate effort. So every decision the Bobtail Yearlings make will be informed by our [five vowels of historical consequence](https://bobtailyearlings.com/essays/ongoing/five-vowels-of-historical-consequence).
+To make history once again, bands need to be the best in the *world*— and this requires deliberate effort. And so every decision the Bobtail Yearlings make will be informed by our [five vowels of historical consequence](https://bobtailyearlings.com/essays/ongoing/five-vowels-of-historical-consequence).
 
 ##### Longevity
 
@@ -31,13 +31,7 @@ As such, the Bobtail Yearlings promise to be a working band for as long as Benne
 
 To avoid sharing their earnings, indie artists spend time and energy on administrative work that would otherwise be handled by business partners. And some take this a step further, by choosing to create without collaboration. But what competitive edge could a solo artist working part time possibly have, to make them the best in the *world*?
 
-And so the Bobtail Yearlings will readily work with business partners, allowing us to focus solely on our craft. We'll also periodically bring in new bandmates to be equal collaborators, allowing us to create works greater than the sum of their parts.
-
-##### Helping others
-
-Mr. Rogers told us that when times are bad, we should [look for the helpers](https://www.goodreads.com/quotes/198594-when-i-was-a-boy-and-i-would-see-scary). Well, times are bad now in rock— so why aren't there more helpers? It's because indie artists need to hustle to be heard. This leaves them with no time or energy to help others.
-
-As such, the Bobtail Yearlings promise to be helpers. We'll do so by working closely with [BYCombo](https://bycombo.com), our rock band incubator.
+This is why the Bobtail Yearlings will readily work with business partners, allowing us to focus solely on our craft. We'll also periodically bring in new bandmates to be equal collaborators, allowing us to create works greater than the sum of their parts.
 
 ##### Business partners
 
@@ -50,6 +44,12 @@ Meanwhile, the *most* creative works are often backed by those who are largely n
 If the Bobtail Yearlings are to achieve our mission, then others should find it enjoyable to interact with us. And it should feel just as meaningful for us to work with each other. So each of us will vow to be a model of emotional intelligence, who'll treat others with empathy, kindness, and respect.
 
 Of course, we won't hesitate to criticize indie rock as an ideology. But we'll always remind ourselves that those who've devoted their lives to it are people like us, and they're the heroes of their own stories.
+
+##### Helping others
+
+Mr. Rogers told us that when times are bad, we should [look for the helpers](https://www.goodreads.com/quotes/198594-when-i-was-a-boy-and-i-would-see-scary). Well, times are bad now in rock. So why aren't there more helpers? It's because every indie artist's top priority is to be self-empowered— a trait that doesn't lend itself well to helping others.
+
+And so the Bobtail Yearlings promise to be helpers, even if this puts us at greater risk of being exploited. We'll do so by making ourselves available to the bands who come up through [BYCombo](https://bycombo.com), our rock band incubator.
 
 ##### Roles
 
