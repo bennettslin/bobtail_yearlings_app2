603f1a9afbcc90bf34b25cfd61096d91251aaa6d
Bennett Lin <bennettslin@gmail.com>
2023-06-28 05:11:21 -0700

    More mission revisions.

--- src/pages/essays/ongoing/our-mission-principles-and-terms.jsx
+++ src/pages/essays/ongoing/our-mission-principles-and-terms.jsx
@@ -20,7 +20,7 @@ const Component = () => (
 
 In the past, the rock music of the English-speaking world was [*the* canon](https://www.rollingstone.com/music/music-lists/100-greatest-artists-147446). It was always the most original, and often the most accomplished. But today's indie rock isn't better than any foreign canon out there, which is why it seems so inconsequential.
 
-To make history once again, bands need to be the best in the *world*— and this requires deliberate effort. So every decision the Bobtail Yearlings make will be informed by our [five vowels of historical consequence](https://bobtailyearlings.com/essays/ongoing/five-vowels-of-historical-consequence).
+To make history once again, bands need to be the best in the *world*— and this requires deliberate effort. So every artistic decision the Bobtail Yearlings make will be informed by our [five vowels of historical consequence](https://bobtailyearlings.com/essays/ongoing/five-vowels-of-historical-consequence).
 
 ##### Longevity
 
@@ -48,7 +48,7 @@ And so the Bobtail Yearlings prefer to work with business partners who take prid
 
 ##### Emotional intelligence
 
-If the Bobtail Yearlings are to achieve our mission, then others should find it rewarding to interact with us. And it should feel just as meaningful for us to work with each other. So each Yearling will vow to be a model of emotional intelligence, treating others with empathy, kindness, and respect.
+If the Bobtail Yearlings are to achieve our mission, then others should find it rewarding to interact with us. And it should feel just as meaningful for us to work with each other. So each Yearling will strive to be a model of emotional intelligence, treating others with empathy, kindness, and respect.
 
 Of course, we won't hesitate to criticize indie rock as an ideology. But we'll always remind ourselves that those who've devoted their lives to it are people like us, and they're the heroes of their own stories.
 
@@ -56,7 +56,7 @@ Of course, we won't hesitate to criticize indie rock as an ideology. But we'll a
 
 Mr. Rogers once told us that when times are bad, we can [look for the helpers](https://www.goodreads.com/quotes/198594). Well, times are bad in rock now… so why aren't there more helpers? It's because indie artists are celebrated for empowering themselves— and self-empowerment isn't a trait that lends itself to helping others.
 
-But the Bobtail Yearlings promise to be a helper. We'll do so by being generous with our creative tools, such as [*Bobtail Method*](https://bobtailmethod.com) and [*Bobtail Dominoes*](https://bobtaildominoes.com), and by collaborating with up-and-coming [BYCombo](https://bycombo.com) bands. In the long term, we'll draw from band funds to create scholarships for BYCombo's most promising artists.
+But the Bobtail Yearlings promise to be a helper. We'll do so by being generous with our creative tools, such as [*Bobtail Method*](https://bobtailmethod.com) and [*Bobtail Dominoes*](https://bobtaildominoes.com), and by collaborating with up-and-coming [BYCombo](https://bycombo.com) bands. In the long term, we'll help create scholarships for BYCombo's most promising artists.
 
 ### Our band terms
 
@@ -78,7 +78,9 @@ For each artistic project, a bandmate or two will be assigned the role of projec
 
 Everyone who's a Yearling during the creation of a project will be given equal credit and royalties. This includes the admin, as well as artists of other mediums. Of course, we're free to mention who contributed which parts, as Lennon and McCartney often did.
 
-Earnings from live performances, as well as royalties from [*Yearling's Bobtail*](https://www.yearlingsbobtail.com) and our extracurricular projects, will first be split evenly amongst new bandmates to grow their savings, up to a fixed limit. The rest will then be split evenly amongst all bandmates.
+Earnings from live performances and our extracurricular projects will first be split evenly amongst new bandmates to grow their savings, up to a fixed limit. The rest will then be split evenly amongst all bandmates.
+
+Since [*Yearling's Bobtail*](https://www.yearlingsbobtail.com) was made while Bennett was the sole bandmate, its royalties will be treated like earnings from an extracurricular project.
 
 ##### Recruitment
 
