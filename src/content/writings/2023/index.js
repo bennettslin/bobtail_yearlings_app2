17fe7711d820a3390737a4923b5da362be3c63e9
Bennett Lin <bennettslin@gmail.com>
2024-01-09 01:13:54 -0800

    Remove some writings.

--- src/content/writings/2023/index.js
+++ src/content/writings/2023/index.js
@@ -1,23 +1,7 @@
 import { getMapFromPageConfigs } from '../../../utils/pages/config'
 import { WRITINGS_PAGE } from '../../../constants/pages'
 
-export const pageConfigs = [
-    {
-        id: `revolutions-start-when-theyre-allowed`,
-        title: `Revolutions start when they're allowed`,
-        date: { year: 2023, month: 3 },
-    },
-    // {
-    //     id: `science-explains-why-todays-rock-is-worse`,
-    //     title: `Science explains why today's rock is worse`,
-    //     date: { year: 2023, month: 3 },
-    // },
-    // {
-    //     id: `the-case-for-yearlings-bobtail`,
-    //     title: `The case for *Yearling's Bobtail*`,
-    //     date: { year: 2023, month: 3 },
-    // },
-]
+export const pageConfigs = []
 
 export const pagesMap = getMapFromPageConfigs({
     topLevelPage: WRITINGS_PAGE,
